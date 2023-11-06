a936f27858610f12b9da396e3ba824fff0cdd75d
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-07 01:41:49 +0800

    amend getMessages

--- getMessages.py
+++ getMessages.py
@@ -4,7 +4,7 @@ import json
 @app.route('/api/getMessages', methods=['GET'])
 def getMessages():
     data = {
-        "message":"nothing to see here."
+        "message":"nothing to see here. Add something at the end."
     }
     response = app.response_class(response=json.dumps(data),
                                   status=200,
