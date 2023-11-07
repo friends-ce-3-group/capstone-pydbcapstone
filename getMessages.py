98b680b9c53f2257ad38426751b042151b88f95e
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-07 10:24:19 +0800

    push latest image tag to variables

--- getMessages.py
+++ getMessages.py
@@ -4,7 +4,7 @@ import json
 @app.route('/api/getMessages', methods=['GET'])
 def getMessages():
     data = {
-        "message":"how to reliably update this?"
+        "message":"how to reliably update this? Almost there...?"
     }
     response = app.response_class(response=json.dumps(data),
                                   status=200,
