2d3bbbfd760470abcab8351a702a538120e51050
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-10 20:43:27 +0800

    swap

--- testDBconnect.py
+++ testDBconnect.py
@@ -5,12 +5,21 @@ import mysql.connector
 
 @app.route('/api/testdb', methods=['GET'])
 def testdb():
-    data = getEndPoint.get_rds_endpoint()
+    # data = getEndPoint.get_rds_endpoint()
+
+    # response = app.response_class(response=json.dumps(data),
+    #                               status=200,
+    #                               mimetype='application/json')
+    
+    data = {}
+    data["ENDPOINT"] = app.config["ENDPOINT"]
+    data["DBUSER"] = app.config["DBUSER"]
+    data["DBPASS"] = app.config["DBPASS"]
 
     response = app.response_class(response=json.dumps(data),
                                   status=200,
                                   mimetype='application/json')
-    
+
     return response
 
 @app.route('/api/testConfig', methods=['GET'])
