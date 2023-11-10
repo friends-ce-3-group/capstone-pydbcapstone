1ce71bdbf6554de0b0ea3d56246afeae5203b724
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-10 21:36:27 +0800

    remove unneeded deps

--- testDBconnect.py
+++ testDBconnect.py
@@ -3,28 +3,6 @@ import json
 import getEndPoint
 import mysql.connector
 
-@app.route('/api/testdb', methods=['GET'])
-def testdb():
-    data = getEndPoint.get_rds_endpoint()
-
-    response = app.response_class(response=json.dumps(data),
-                                  status=200,
-                                  mimetype='application/json')
-
-    return response
-
-# @app.route('/api/testConfig', methods=['GET'])
-# def testConfig():
-#     data = {}
-#     data["ENDPOINT"] = app.config["ENDPOINT"]
-#     data["DBUSER"] = app.config["DBUSER"]
-#     data["DBPASS"] = app.config["DBPASS"]
-
-#     response = app.response_class(response=json.dumps(data),
-#                                   status=200,
-#                                   mimetype='application/json')
-    
-#     return response
 
 @app.route('/api/getCardsTable', methods=['GET'])
 def createTable():
