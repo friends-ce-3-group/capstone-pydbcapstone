dc26dc7ffe45a0b87bc4de713decfc7ebdd595d5
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-11 18:31:13 +0800

    back to orig

--- testDBconnect.py
+++ testDBconnect.py
@@ -1,14 +1,39 @@
 from app import app
 import json
-from DBConnect import dbConnector
-
+# from DBConnect import dbConnector
+import mysql.connector
 
 @app.route('/api/getCardsTable', methods=['GET'])
 def createTable():
 
     query = """SELECT * FROM Cards"""
 
-    result = dbConnector.execute(query)
+    # result = dbConnector.execute(query)
+
+
+    connection = mysql.connector.connect(
+            host=app.config["ENDPOINT"],
+            user=app.config["DBUSER"],
+            password=app.config["DBPASS"],
+            database=app.config["DBNAME"]
+        )
+
+    cursor = connection.cursor()
+
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+
+
+
+
+
+
+
+
+
 
     response = app.response_class(response=json.dumps(result),
                                 status=200,
@@ -18,15 +43,3 @@ def createTable():
 
 
 
-@app.route('/api/listCardsTable', methods=['GET'])
-def createTable():
-
-    query = """SELECT * FROM Cards"""
-
-    result = dbConnector.execute(query)
-
-    response = app.response_class(response=json.dumps(result),
-                                status=200,
-                                mimetype='application/json')
-
-    return response
