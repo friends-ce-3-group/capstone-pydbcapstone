944ee5acaeb48725c5ff46958ea37906737c159a
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-11 21:40:01 +0800

    implement /api/cardsCatalog

--- cardsCatalog.py
+++ cardsCatalog.py
@@ -1,39 +1,19 @@
 from app import app
 import json
+from DBConnect import dbConnector
+
+
 
 
 @app.route('/api/cardsCatalog', methods=['GET'])
 def cardsCatalog():
-    data = [
-        {
-        "key": "farewell1",
-        "category": "Farewell",
-        "path": "farewell1.jpg",
-        "backgroundColor": "#002744"
-        },
-        {
-        "key": "birthday1",
-        "category": "Birthday",
-        "path": "birthday1.jpg",
-        "backgroundColor": "#ffffff"
-        },
-        {
-        "key": "wedding1",
-        "category": "Wedding",
-        "path": "wedding1.jpg",
-        "backgroundColor": "#FFFFFF"
-        },
-        {
-        "key": "anniversary1",
-        "category": "Anniversary",
-        "path": "anniversary1.jpg",
-        "backgroundColor": "#FFFFFF"
-        },
-    ]
 
-    response = app.response_class(response=json.dumps(data),
-                                  status=200,
-                                  mimetype='application/json')
+    query = """SELECT * FROM CardsCatalog"""
+
+    result = dbConnector.read(query)
+
+    response = app.response_class(response=json.dumps(result),
+                                status=200,
+                                mimetype='application/json')
     
-
     return response
