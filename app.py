1dfbfae1063bac8b65538cc2a41d3244622cf8ee
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-12 12:13:36 +0800

    try separating definitions again

--- app.py
+++ app.py
@@ -33,30 +33,30 @@ class DBConnect:
 db = DBConnect(app)
 
 
-@app.route('/api/cardsCatalog', methods=['GET'])
-def cardsCatalog():
+# @app.route('/api/cardsCatalog', methods=['GET'])
+# def cardsCatalog():
 
-    query = """SELECT * FROM cardsCatalog"""
+#     query = """SELECT * FROM cardsCatalog"""
 
-    result = []
+#     result = []
 
-    try:
-        result = db.read(query)
-        status_code = 200
-    except Exception as err:
-        result = err
-        status_code = 400 # The request could not be understood by the server due to incorrect syntax
+#     try:
+#         result = db.read(query)
+#         status_code = 200
+#     except Exception as err:
+#         result = err
+#         status_code = 400 # The request could not be understood by the server due to incorrect syntax
 
-    response = app.response_class(response=json.dumps(result),
-                                status=status_code,
-                                mimetype='application/json')
+#     response = app.response_class(response=json.dumps(result),
+#                                 status=status_code,
+#                                 mimetype='application/json')
 
-    return response
-
-
+#     return response
 
 
-# import cardsCatalog
+
+
+import cardsCatalog
 
 # import createCard
 
--- cardsCatalog.py
+++ cardsCatalog.py
@@ -1,17 +1,22 @@
-# import json
-# from app import app
+from app import db, app
+import json
 
-# @app.route('/api/cardsCatalog', methods=['GET'])
-# def cardsCatalog():
+@app.route('/api/cardsCatalog', methods=['GET'])
+def cardsCatalog():
 
-#     from DBConnect import db_read
+    query = """SELECT * FROM cardsCatalog"""
 
-#     query = """SELECT * FROM cardsCatalog"""
+    result = []
 
-#     result = db_read(query)
+    try:
+        result = db.read(query)
+        status_code = 200
+    except Exception as err:
+        result = err
+        status_code = 400 # The request could not be understood by the server due to incorrect syntax
 
-#     response = app.response_class(response=json.dumps(result),
-#                                 status=200,
-#                                 mimetype='application/json')
+    response = app.response_class(response=json.dumps(result),
+                                status=status_code,
+                                mimetype='application/json')
 
-#     return response
+    return response
