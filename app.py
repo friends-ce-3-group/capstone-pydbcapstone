576fd4d1ca91d04db14fbf21a97bf16ccb09f894
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-12 10:22:19 +0800

    reorg includes

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 # app.py
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py") # run this before any of the other imports to set up the configuration data
 
-import DBConnect # run this to initialize the DB
+class DBConnector:
+    def __init__(self, app):
+        self.connection = mysql.connector.connect(
+            host=app.config["ENDPOINT"],
+            user=app.config["DBUSER"],
+            password=app.config["DBPASS"],
+            database=app.config["DBNAME"]
+        )
+
+    def read(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        return result
+
+dbConnector = DBConnector(app)
+
 
 import cardsCatalog
 
--- cardsCatalog.py
+++ cardsCatalog.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, dbConnector as db
 import json
 # from DBConnect import dbConnector
 
@@ -8,23 +8,8 @@ def cardsCatalog():
 
     query = """SELECT * FROM cardsCatalog"""
 
-    # result = dbConnector.read(query)
+    result = db.read(query)
 
-    # response = app.response_class(response=json.dumps(result),
-    #                             status=200,
-    #                             mimetype='application/json')
-
-    import mysql.connector
-
-    connection = mysql.connector.connect(
-        host=app.config["ENDPOINT"],
-        user=app.config["DBUSER"],
-        password=app.config["DBPASS"],
-        database=app.config["DBNAME"]
-    )
-    cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
     response = app.response_class(response=json.dumps(result),
                                 status=200,
                                 mimetype='application/json')
