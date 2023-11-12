fbfd642e0e3e95e71c3c8a02ff9567d46f8ba643
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-12 10:10:51 +0800

    simplify again

--- cardsCatalog.py
+++ cardsCatalog.py
@@ -1,6 +1,6 @@
 from app import app
 import json
-from DBConnect import dbConnector
+# from DBConnect import dbConnector
 
 
 @app.route('/api/cardsCatalog', methods=['GET'])
@@ -8,10 +8,25 @@ def cardsCatalog():
 
     query = """SELECT * FROM cardsCatalog"""
 
-    result = dbConnector.read(query)
+    # result = dbConnector.read(query)
 
+    # response = app.response_class(response=json.dumps(result),
+    #                             status=200,
+    #                             mimetype='application/json')
+
+    import mysql.connector
+
+    connection = mysql.connector.connect(
+        host=app.config["ENDPOINT"],
+        user=app.config["DBUSER"],
+        password=app.config["DBPASS"],
+        database=app.config["DBNAME"]
+    )
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
     response = app.response_class(response=json.dumps(result),
                                 status=200,
                                 mimetype='application/json')
-    
+
     return response
