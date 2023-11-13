3ba2b3a09dfa275aa8175065547c764971b5c760
Justin Lim <goodaccoustics@hotmail.com>
2023-11-13 10:26:58 +0800

    add cors headers to cardsCatalog

--- cardsCatalog.py
+++ cardsCatalog.py
@@ -1,25 +1,28 @@
 from app import db, app
+import cors
 import json
 
-@app.route('/api/cardsCatalog', methods=['GET'])
+@app.route('/api/cardsCatalog', methods=['OPTIONS','GET'])
 def cardsCatalog():
+    if request.method == "OPTIONS":
+        return cors_preflight_response()
+    else:            
+        query = """SELECT * FROM cardsCatalog"""
 
-    query = """SELECT * FROM cardsCatalog"""
+        data = {}
+        status_code = 500
 
-    data = {}
-    status_code = 500
+        try:
+            data = db.read(query)
 
-    try:
-        data = db.read(query)
+            status_code = 200
 
-        status_code = 200
-
-    except Exception as err:
-        data = { "Error": str(err) }
+        except Exception as err:
+            data = { "Error": str(err) }
 
 
-    response = app.response_class(response=json.dumps(data),
-                                status=status_code,
-                                mimetype='application/json')
+        response = app.response_class(response=json.dumps(data),
+                                    status=status_code,
+                                    mimetype='application/json')
 
-    return response
+        return response
