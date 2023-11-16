626bb745872b087d3583b73b0ebb4049fcc0cb39
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-17 03:28:54 +0800

    remove time conversion

--- scheduledEvents.py
+++ scheduledEvents.py
@@ -32,11 +32,14 @@ def create_cloudwatch_event_rule(rule_name, cron_expression, role_arn, lambda_fu
     return response
 
 def utc_cron_generator(sg_min, sg_hr, sg_day_of_month, sg_month, sg_day_of_week, sg_year):
-    hr = int(sg_hr) - 8
-    day = int(sg_day_of_month)
-    if hr < 0:
-        hr = hr + 24
-        day = day - 1
+    # hr = int(sg_hr) - 8
+    # day = int(sg_day_of_month)
+    # if hr < 0:
+    #     hr = hr + 24
+    #     day = day - 1
+
+    hr = sg_hr
+    day = sg_day_of_month
 
     cron_expression = "cron({} {} {} {} {} {})".format(sg_min, hr, day, sg_month, sg_day_of_week, sg_year)
     return cron_expression
