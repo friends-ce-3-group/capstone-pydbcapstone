68efb06a0cc013f91f3c50f83220d0a76255d5e4
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-18 00:18:36 +0800

    clean up

--- sendCard.py
+++ sendCard.py
@@ -15,31 +15,6 @@ def sendCard():
 
     data, status_code = sendCardImpl(cardId, app)
 
-    # try:
-
-    #     lambda_function_arn = app.config["LAMBDAARN"]
-    #     role_arn = app.config["EVENTBRIDGEIAMROLEARN"]
-    #     access_key_id = app.config["ACCESS_KEY_ID"]
-    #     access_key = app.config["ACCESS_KEY"]
-
-    #     now = datetime.now() + timedelta(seconds=30)
-
-    #     key = "420e9f81-a1e7-4cb0-a945-a9208104ad5c"
-
-    #     datetime_to_send = utc_cron_generator(now)
-
-    #     schedule_name = "friends-capstone-send-cards-{}".format(key)
-
-    #     payload = '{"hey": "cm"}'
-
-    #     data = create_cloudwatch_event_rule(schedule_name, datetime_to_send, role_arn, lambda_function_arn, payload, access_key_id, access_key)
-        
-    #     status_code = statuscodes.STATUS_OK
-
-    # except Exception as err:
-    #     data = {"Error" : str(err)}
-    #     status_code = statuscodes.STATUS_ERR
-
     response = app.response_class(response=json.dumps(data),
                                 status=status_code,
                                 mimetype='application/json')
@@ -77,3 +52,31 @@ def sendCardImpl(cardId, app):
         data = create_cloudwatch_event_rule(schedule_name, datetime_to_send, role_arn, lambda_function_arn, payload_json, access_key_id, access_key)
 
     return data, status_code
+
+
+
+
+    # try:
+
+    #     lambda_function_arn = app.config["LAMBDAARN"]
+    #     role_arn = app.config["EVENTBRIDGEIAMROLEARN"]
+    #     access_key_id = app.config["ACCESS_KEY_ID"]
+    #     access_key = app.config["ACCESS_KEY"]
+
+    #     now = datetime.now() + timedelta(seconds=30)
+
+    #     key = "420e9f81-a1e7-4cb0-a945-a9208104ad5c"
+
+    #     datetime_to_send = utc_cron_generator(now)
+
+    #     schedule_name = "friends-capstone-send-cards-{}".format(key)
+
+    #     payload = '{"hey": "cm"}'
+
+    #     data = create_cloudwatch_event_rule(schedule_name, datetime_to_send, role_arn, lambda_function_arn, payload, access_key_id, access_key)
+        
+    #     status_code = statuscodes.STATUS_OK
+
+    # except Exception as err:
+    #     data = {"Error" : str(err)}
+    #     status_code = statuscodes.STATUS_ERR
