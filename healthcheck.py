c9b05d5cd7e4b6b8d67c895963dccc2454c64211
Low Chee Meng <lowcheemeng02@gmail.com>
2023-12-01 20:08:54 +0800

    update healthcheck again

--- healthcheck.py
+++ healthcheck.py
@@ -6,6 +6,8 @@ def healthcheck():
     data = {"Performing healthcheck" : "/api/healthcheck"}
     status_code = statuscodes.STATUS_OK
 
+    print("Performing healthcheck")
+
     response = app.response_class(response=json.dumps(data),
                                   status=status_code,
                                   mimetype='application/json')
