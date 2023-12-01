b693d524b031c593110933086f5c5c73982fcd01
Low Chee Meng <lowcheemeng02@gmail.com>
2023-12-01 22:06:54 +0800

    try rds proxy again

--- DBConnect.py
+++ DBConnect.py
@@ -4,7 +4,7 @@ import statuscodes
 class DBConnector:
     def __init__(self, app):
         self.connection = connector.connect(
-            host=app.config["ENDPOINT"],
+            host="friends-capstone-rds-rds-dbproxy.proxy-ccbqfspyybpn.us-west-2.rds.amazonaws.com",
             user=app.config["DBUSER"],
             password=app.config["DBPASS"],
             database=app.config["DBNAME"],
